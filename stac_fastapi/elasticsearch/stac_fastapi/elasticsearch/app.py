aea9d52c5964218d2806527df9231257a2f7503b
jonhealy1 <jonathan.d.healy@gmail.com>
2022-04-15 03:55:02 -0700

    get collection by id

--- stac_fastapi/elasticsearch/stac_fastapi/elasticsearch/app.py
+++ stac_fastapi/elasticsearch/stac_fastapi/elasticsearch/app.py
@@ -11,9 +11,11 @@ app = Robyn(__file__)
 async def h(requests):
     return "Hello, world!"
 
+class Request():
+    base_url = str
+
 @app.post("/collections")
 async def create_collection(request):
-
     await IndexesClient().create_indexes()
     client = TransactionsClient()
     collection = json.loads(bytearray(request["body"]).decode("utf-8"))
@@ -22,19 +24,20 @@ async def create_collection(request):
     await client.create_collection(collection=collection, request=Request)
     return json.dumps(collection)
 
-class Request():
-    base_url = str
+@app.get("/collections/:collection_id")
+async def get_collection(request):
+    client = CoreClient()
+    Request.base_url = "localhost:8080"
+    collection = await client.get_collection(collection_id=request["params"]["collection_id"], request=Request)
+    return json.dumps(collection)
 
 @app.get("/collections")
 async def get_all_collections(requests):
     client = CoreClient()
     Request.base_url = "localhost:8080"
-    
-    # request["base_url"] = "localhost:8080"
     collections = await client.all_collections(request=Request)
     return json.dumps({
         "collections": collections
     })
-    return json.dumps(requests)
 
 app.start(port=8080, url="0.0.0.0")
